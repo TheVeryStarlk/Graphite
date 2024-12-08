0190d177d63ade162a16c32dc9d4d08e5cce4fba
Starlk <starlkytminecraft@gmai.com>
2024-12-08 22:15:21 +0300

    Add service collection extension.

--- Graphite.Example/Program.cs
+++ Graphite.Example/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, Graphite!");
+﻿using Graphite.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder();
+
+builder.Logging.SetMinimumLevel(LogLevel.Trace);
+
+builder.Services.AddGraphite();
+
+var host = builder.Build();
+
+host.Run();
---
+++ Graphite/Hosting/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Graphite.Hosting;
+
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection AddGraphite(this IServiceCollection services)
+	{
+		return services;
+	}
+}
