78e07fc158b842aac1c65876d9ee46c03c6e0259
TheVeryStarlk <starlkytminecraft@gmai.com>
2024-12-18 01:22:20 +0300

    Enable AOT.

--- Graphite/Hosting/ServiceCollectionExtensions.cs
+++ Graphite/Hosting/ServiceCollectionExtensions.cs
@@ -1,4 +1,4 @@
-﻿using Graphite.Abstractions;
+﻿using System.Diagnostics.CodeAnalysis;
 using Graphite.Abstractions.Eventing;
 using Graphite.Abstractions.Worlds;
 using Graphite.Eventing;
@@ -13,7 +13,7 @@ namespace Graphite.Hosting;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddGraphite<T>(this IServiceCollection services) where T : Controller
+    public static IServiceCollection AddGraphite<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] T>(this IServiceCollection services) where T : Controller
     {
         services.AddHostedService<WorkerService>();
 
